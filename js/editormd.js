f1e7720de45edd0ef76ce69d33ae85666e38d0a5
JaxsonWang <wangjin@boxrom.com>
2017-06-09 10:55:14 +0800

    解决XSS脚本攻击

--- js/editormd.js
+++ js/editormd.js
@@ -3831,14 +3831,16 @@
         {
             var tag = filterTags[i];
 
-            html = html.replace(new RegExp("\<\s*" + tag + "\s*([^\>]*)\>([^\>]*)\<\s*\/" + tag + "\s*\>", "igm"), "");
+            //适配单标签和闭合标签
+            html = html.replace(new RegExp("\<\s*" + tag + "\s*([^\>]*)\>([^\>]*)\<\s*\/" + tag + "\s*\>|\<\s*([^\>]*)\s(.*?)\>", "igm"), "");
         }
         
         //return html;
 
         if (typeof attrs !== "undefined")
         {
-            var htmlTagRegex = /\<(\w+)\s*([^\>]*)\>([^\>]*)\<\/(\1)\>/ig;
+            var htmlTagRegex = /\<(\w+)\s*([^\>]*)\>([^\>]*)\<\/(\1)\>|\<\s*([^\>]*)\s(.*?)\>/ig;
+            //var go = /\<\s*([^\>]*)\s(.*?)\>/ig;
 
             if (attrs === "*")
             {
