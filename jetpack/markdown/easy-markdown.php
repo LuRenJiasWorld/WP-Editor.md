28bc42b7203257b2e77868c623d1fc872b07378e
JaxsonWang <wangjin@boxrom.com>
2017-06-10 09:52:52 +0800

    解决入库XSS跨站攻击

--- jetpack/markdown/easy-markdown.php
+++ jetpack/markdown/easy-markdown.php
@@ -31,6 +31,8 @@ Author URI: http://automattic.com/
  * **********************************************************************
  */
 
+require('xsshtml.class.php');
+
 class WPCom_Markdown {
 
 
@@ -579,39 +581,10 @@ class WPCom_Markdown {
 		if ( $args['unslash'] )
 			$text = wp_slash( $text );
 
-		/**
-		 * xss过滤函数
-		 *
-		 * @param $text
-		 * @return string
-		 */
-		function remove_xss($text) {
-			$text = preg_replace('/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]+/S', '', $text);
+		//过滤标签
+		$xss = new XssHtml($text);
 
-			$parm1 = Array('javascript', 'vbscript', 'expression', 'applet', 'meta', 'xml', 'blink', 'link', 'script', 'embed', 'object', 'iframe', 'frame', 'frameset', 'ilayer', 'layer', 'bgsound', 'title', 'base');
-
-			$parm2 = Array('onabort', 'onactivate', 'onafterprint', 'onafterupdate', 'onbeforeactivate', 'onbeforecopy', 'onbeforecut', 'onbeforedeactivate', 'onbeforeeditfocus', 'onbeforepaste', 'onbeforeprint', 'onbeforeunload', 'onbeforeupdate', 'onblur', 'onbounce', 'oncellchange', 'onchange', 'onclick', 'oncontextmenu', 'oncontrolselect', 'oncopy', 'oncut', 'ondataavailable', 'ondatasetchanged', 'ondatasetcomplete', 'ondblclick', 'ondeactivate', 'ondrag', 'ondragend', 'ondragenter', 'ondragleave', 'ondragover', 'ondragstart', 'ondrop', 'onerror', 'onerrorupdate', 'onfilterchange', 'onfinish', 'onfocus', 'onfocusin', 'onfocusout', 'onhelp', 'onkeydown', 'onkeypress', 'onkeyup', 'onlayoutcomplete', 'onload', 'onlosecapture', 'onmousedown', 'onmouseenter', 'onmouseleave', 'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup', 'onmousewheel', 'onmove', 'onmoveend', 'onmovestart', 'onpaste', 'onpropertychange', 'onreadystatechange', 'onreset', 'onresize', 'onresizeend', 'onresizestart', 'onrowenter', 'onrowexit', 'onrowsdelete', 'onrowsinserted', 'onscroll', 'onselect', 'onselectionchange', 'onselectstart', 'onstart', 'onstop', 'onsubmit', 'onunload');
-
-			$parm = array_merge($parm1, $parm2);
-
-			for ($i = 0; $i < sizeof($parm); $i++) {
-				$pattern = '/';
-				for ($j = 0; $j < strlen($parm[$i]); $j++) {
-					if ($j > 0) {
-						$pattern .= '(';
-						$pattern .= '(&#[x|X]0([9][a][b]);?)?';
-						$pattern .= '|(&#0([9][10][13]);?)?';
-						$pattern .= ')?';
-					}
-					$pattern .= $parm[$i][$j];
-				}
-				$pattern .= '/i';
-				$text = preg_replace($pattern, ' ', $text);
-			}
-			return $text;
-		}
-
-		return remove_xss($text);
+		return $xss->getHtml();
 	}
 
 	/**
