a96481a712b3e1281a774b5bd4a51e80b1d30f51
JaxsonWang <wangjin@boxrom.com>
2017-06-15 10:25:43 +0800

    more过滤

--- Jetpack/markdown/xsshtml_test.php
+++ Jetpack/markdown/xsshtml_test.php
@@ -1,6 +1,6 @@
 <?php
 require('xsshtml.class.php');
-$html = '<p class="editormd-tex"> c = \pm\sqrt{a^2 + b^2}</p>';
+$html = '<!--more-->';
 $xss = new XssHtml($html);
 $html = $xss->getHtml();
 echo $html;
