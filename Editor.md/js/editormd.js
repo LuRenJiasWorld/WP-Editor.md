c48730b0b6286e7484cfbe6bcb816479f3e57eb2
JaxsonWang <wangjin@boxrom.com>
2017-07-21 11:07:31 +0800

    流程图和时序图为空被调用报错

--- Editor.md/js/editormd.js
+++ Editor.md/js/editormd.js
@@ -1532,11 +1532,19 @@
                     return this;
                 }
                 
-                previewContainer.find(".flowchart").flowChart(); 
+                //previewContainer.find(".flowchart").flowChart();
+                //https://github.com/pandao/editor.md/pull/356/files
+                previewContainer.find(".flowchart")
+                    && previewContainer.find(".flowchart").length > 0
+                    && previewContainer.find(".flowchart").flowChart();
             }
 
             if (settings.sequenceDiagram) {
-                previewContainer.find(".sequence-diagram").sequenceDiagram({theme: "simple"});
+                //previewContainer.find(".sequence-diagram").sequenceDiagram({theme: "simple"});
+                //https://github.com/pandao/editor.md/pull/356/files
+                previewContainer.find(".sequence-diagram")
+                    && previewContainer.find(".sequence-diagram").length > 0
+                    && previewContainer.find(".sequence-diagram").sequenceDiagram({theme: "simple"});
             }
                     
             var preview    = $this.preview;
