5cbfeee7d0a4c734f9e28e4765ff9bd5056529f9
JaxsonWang <wangjin@boxrom.com>
2017-08-17 21:20:11 +0800

    删除无用文件

--- Admin/ReduxCore/framework.php
+++ Admin/ReduxCore/framework.php
@@ -405,25 +405,6 @@
                     }
                     // Ajax saving!!!
                     add_action( "wp_ajax_" . $this->args['opt_name'] . '_ajax_save', array( $this, "ajax_save" ) );
-
-                    if ( $this->args['dev_mode'] == true || Redux_Helpers::isLocalHost() == true ) {
-                        require_once 'core/dashboard.php';
-                        new reduxDashboardWidget( $this );
-
-                        if ( ! isset ( $GLOBALS['redux_notice_check'] ) ) {
-                            require_once 'core/newsflash.php';
-
-                            $params = array(
-                                'dir_name'    => 'notice',
-                                'server_file' => 'http://reduxframework.com/wp-content/uploads/redux/redux_notice.json',
-                                'interval'    => 3,
-                                'cookie_id'   => 'redux_blast',
-                            );
-
-                            new reduxNewsflash( $this, $params );
-                            $GLOBALS['redux_notice_check'] = 1;
-                        }
-                    }
                 }
 
                 /**
