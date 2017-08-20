8e005a29223df26ebb44ec716a01f04bc8d42e22
JaxsonWang <wangjin@boxrom.com>
2017-08-20 18:58:28 +0800

    更新V2代码

--- Admin/data/tabs.php
+++ Admin/data/tabs.php
@@ -8,55 +8,55 @@
 // Make sure our temporary variable is empty
 $tabs = array();
 
-$tabs['tab_1'] = array(
+$tabs['basic'] = array(
 	'title'      => __( 'Basic Options', 'iiong' ),
 	'menu_title' => __( 'Basic Options', 'iiong' ),
 	'page'       => 'wpeditormd'
 );
 
-$tabs['tab_2'] = array(
+$tabs['style'] = array(
 	'title'      => __( 'Style Options', 'iiong' ),
 	'menu_title' => __( 'Style Options', 'iiong' ),
 	'page'       => 'wpeditormd'
 );
 
-$tabs['tab_3'] = array(
+$tabs['function'] = array(
 	'title'      => __( 'Function Options', 'iiong' ),
 	'menu_title' => __( 'Function Options', 'iiong' ),
 	'page'       => 'wpeditormd'
 );
 
-$tabs['tab_4'] = array(
+$tabs['prism'] = array(
 	'title'      => __( 'Syntax Highlight Options', 'iiong' ),
 	'menu_title' => __( 'Syntax Highlight Options ', 'iiong' ),
 	'page'       => 'wpeditormd'
 );
 
-$tabs['tab_5'] = array(
+$tabs['emoji'] = array(
 	'title'      => __( 'Emoji Options', 'iiong' ),
 	'menu_title' => __( 'Emoji Options ', 'iiong' ),
 	'page'       => 'wpeditormd'
 );
 
-$tabs['tab_6'] = array(
+$tabs['toc'] = array(
 	'title'      => __( 'ToC Options', 'iiong' ),
 	'menu_title' => __( 'ToC Options ', 'iiong' ),
 	'page'       => 'wpeditormd'
 );
 
-$tabs['tab_7'] = array(
-	'title'      => __( 'LaTeX Options', 'iiong' ),
-	'menu_title' => __( 'LaTeX Options ', 'iiong' ),
+$tabs['katex'] = array(
+	'title'      => __( 'KaTeX Options', 'iiong' ),
+	'menu_title' => __( 'KaTeX Options ', 'iiong' ),
 	'page'       => 'wpeditormd'
 );
 
-$tabs['tab_8'] = array(
+$tabs['flowchart'] = array(
 	'title'      => __( 'FlowChart Options', 'iiong' ),
 	'menu_title' => __( 'FlowChart Options ', 'iiong' ),
 	'page'       => 'wpeditormd'
 );
 
-$tabs['tab_9'] = array(
+$tabs['sequence'] = array(
 	'title'      => __( 'Sequence Options', 'iiong' ),
 	'menu_title' => __( 'Sequence Options ', 'iiong' ),
 	'page'       => 'wpeditormd'
--- Admin/skelet/assets/js/_meow.js
+++ Admin/skelet/assets/js/_meow.js
@@ -1,18 +1,19 @@
 (function ($,doc) {
     jQuery(document).ready(function ($) {
+        /**
+         * 隐藏内容
+         * @type {Element}
+         */
         var support_comment = doc.querySelector(".support_comment");
         var toc_tips = doc.querySelector(".toc_tips");
-
         if ( support_comment !== null) {
             var support_comment_text = getNearEle(support_comment, 0);
             support_comment_text.parentNode.removeChild(support_comment_text);
         }
-
         if ( toc_tips !== null) {
             var toc_tips_text = getNearEle(toc_tips, 0);
             toc_tips_text.parentNode.removeChild(toc_tips_text);
         }
-
         /**
          * 获取相邻元素
          * @param ele 参考物元素
@@ -45,5 +46,10 @@
         function next() {
             return getNearEle(this, 0);
         }
+
+        /**
+         * 根据选项显示或者隐藏
+         */
+        doc.attributes
     });
 })(jQuery,document);
