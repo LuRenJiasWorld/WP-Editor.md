44eb3ca0ec8a99f1b0209b86674a8c273193daf8
JaxsonWang <wangjin@boxrom.com>
2017-08-22 17:43:48 +0800

    更新翻译

--- editormd_options.php
+++ editormd_options.php
@@ -13,7 +13,7 @@ skelet_dir( WP_EDITORMD_PLUGIN_PATH . '/Admin/data/' );
 
 function options_script() {
 	wp_enqueue_script( 'meow', WP_EDITORMD_PLUGIN_URL . '/Admin/skelet/assets/js/_meow.js', array(), WP_EDITORMD_PLUGIN_VERSION, true );
-	wp_enqueue_style( 'meow', WP_EDITORMD_PLUGIN_URL . '/Admin/skelet/assets/css/_meow.css', array(), WP_EDITORMD_PLUGIN_VERSION, false );
+	wp_enqueue_style( 'meow', WP_EDITORMD_PLUGIN_URL . '/Admin/skelet/assets/css/_meow.css', array(), WP_EDITORMD_PLUGIN_VERSION, 'all' );
 }
 
 add_action( 'admin_print_scripts', 'options_script' );
