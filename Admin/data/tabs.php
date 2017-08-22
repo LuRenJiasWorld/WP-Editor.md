ac09575171829ed5590013045b164849443dd588
JaxsonWang <wangjin@boxrom.com>
2017-08-22 22:44:45 +0800

    翻译失效的问题

--- Admin/data/tabs.php
+++ Admin/data/tabs.php
@@ -28,43 +28,43 @@ $tabs['function'] = array(
 
 $tabs['prism'] = array(
 	'title'      => __( 'Syntax Highlight Options', 'editormd' ),
-	'menu_title' => __( 'Syntax Highlight Options ', 'editormd' ),
+	'menu_title' => __( 'Syntax Highlight Options', 'editormd' ),
 	'page'       => 'wpeditormd'
 );
 
 $tabs['emoji'] = array(
 	'title'      => __( 'Emoji Options', 'editormd' ),
-	'menu_title' => __( 'Emoji Options ', 'editormd' ),
+	'menu_title' => __( 'Emoji Options', 'editormd' ),
 	'page'       => 'wpeditormd'
 );
 
 $tabs['toc'] = array(
 	'title'      => __( 'ToC Options', 'editormd' ),
-	'menu_title' => __( 'ToC Options ', 'editormd' ),
+	'menu_title' => __( 'ToC Options', 'editormd' ),
 	'page'       => 'wpeditormd'
 );
 
 $tabs['katex'] = array(
 	'title'      => __( 'KaTeX Options', 'editormd' ),
-	'menu_title' => __( 'KaTeX Options ', 'editormd' ),
+	'menu_title' => __( 'KaTeX Options', 'editormd' ),
 	'page'       => 'wpeditormd'
 );
 
 $tabs['flowchart'] = array(
 	'title'      => __( 'FlowChart Options', 'editormd' ),
-	'menu_title' => __( 'FlowChart Options ', 'editormd' ),
+	'menu_title' => __( 'FlowChart Options', 'editormd' ),
 	'page'       => 'wpeditormd'
 );
 
 $tabs['sequence'] = array(
 	'title'      => __( 'Sequence Options', 'editormd' ),
-	'menu_title' => __( 'Sequence Options ', 'editormd' ),
+	'menu_title' => __( 'Sequence Options', 'editormd' ),
 	'page'       => 'wpeditormd'
 );
 
 $tabs['other'] = array(
 	'title'      => __( 'Other Page', 'editormd' ),
-	'menu_title' => __( 'Other Page ', 'editormd' ),
+	'menu_title' => __( 'Other Page', 'editormd' ),
 	'page'       => 'wpeditormd'
 );
 
