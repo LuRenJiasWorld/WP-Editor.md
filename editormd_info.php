df8ab2dc71a0071fcbf59b5d4978c7fc265faa95
JaxsonWang <wangjin@boxrom.com>
2017-09-29 20:05:57 +0800

    更新描点逻辑

--- editormd_info.php
+++ editormd_info.php
@@ -2,14 +2,17 @@
 
 function woo_upgrade_notice() {
 
-	printf( '
+	if (!!get_option('wp-editormd_options') != 1) {
+		printf( '
 			<div id="message" class="notice-info notice is-dismissible aioseop-notice woo-upgrade-notice">
 				<p>
-					<strong>%1$s</strong>  %2$s
+					<strong>%1$s</strong>%2$s
 				</p>
 			</div>',
-		__( 'Welcome to WordPress Markdown Editor.', 'editormd' ),
-		__( 'Please turn off other markdown editor features, including the Jetpack settings in the Writing-Composing function (if you install the Jetpack plugin).', 'editormd' ));
+			__( 'Welcome to WordPress Markdown Editor.', 'editormd' ),
+			__( 'Please turn off other markdown editor features, including the Jetpack settings in the Writing-Composing function (if you install the Jetpack plugin).', 'editormd' ));
+
+	}
 }
 
 add_action('all_admin_notices', 'woo_upgrade_notice');
