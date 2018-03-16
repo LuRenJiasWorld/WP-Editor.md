0d5436d1752173d75093b734da82c460c63dd6d1
Jaxson Wang <wangjin@boxrom.com>
2018-03-16 16:20:02 +0800

    修复公式小于号失效的问题

--- Jetpack/latex/latex.php
+++ Jetpack/latex/latex.php
@@ -18,11 +18,60 @@
  *
  */
 
+function wp_html_split_iiong( $input ) {
+    return preg_split( get_html_split_regex_iiong(), $input, -1, PREG_SPLIT_DELIM_CAPTURE );
+}
+
+function get_html_split_regex_iiong() {
+    static $regex;
+
+    if ( ! isset( $regex ) ) {
+        $comments =
+            '!'           // Start of comment, after the <.
+            . '(?:'         // Unroll the loop: Consume everything until --> is found.
+            .     '-(?!->)' // Dash not followed by end of comment.
+            .     '[^\-]*+' // Consume non-dashes.
+            . ')*+'         // Loop possessively.
+            . '(?:-->)?';   // End of comment. If not found, match all input.
+
+        $cdata =
+            '!\[CDATA\['  // Start of comment, after the <.
+            . '[^\]]*+'     // Consume non-].
+            . '(?:'         // Unroll the loop: Consume everything until ]]> is found.
+            .     '](?!]>)' // One ] not followed by end of comment.
+            .     '[^\]]*+' // Consume non-].
+            . ')*+'         // Loop possessively.
+            . '(?:]]>)?';   // End of comment. If not found, match all input.
+
+        $escaped =
+            '(?='           // Is the element escaped?
+            .    '!--'
+            . '|'
+            .    '!\[CDATA\['
+            . ')'
+            . '(?(?=!-)'      // If yes, which type?
+            .     $comments
+            . '|'
+            .     $cdata
+            . ')';
+
+        $regex =
+            '/('              // Capture the entire match.
+            .     '<<'           // Find start of element.
+            .     '(?'          // Conditional expression follows.
+            .         $escaped  // Find end of escaped element.
+            .     '|'           // ... else ...
+            .         '[^>]*>?' // Find end of normal element.
+            .     ')'
+            . ')/';
+    }
+
+    return $regex;
+}
+
 function latex_markup_editormd( $content ) {
 
-    $content = preg_replace("/</U","&lt;",$content);
-
-	$textarr = wp_html_split( $content );
+	$textarr = wp_html_split_iiong( $content );
 	
 	$regex = '%
 		\$\$(?:=*|\s+)
