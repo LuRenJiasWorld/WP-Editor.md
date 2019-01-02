241296a8bc9dc756c735ec6543e8073ac1a70ea8
JaxsonWang <i@iiong.com>
2019-01-02 16:32:45 +0800

    update 10.0.3

--- src/Admin/Controller.php
+++ src/Admin/Controller.php
@@ -58,6 +58,38 @@ class Controller {
 		if ( $GLOBALS['wp_version'] >= '5.0' ) {
 			add_filter( 'gutenberg_can_edit_post_type', array($this, 'disable_gutenberg'), 10, 2 );
 			add_filter( 'use_block_editor_for_post_type', array($this,'disable_gutenberg'), 10, 2 );
+
+			remove_action( 'admin_menu', 'gutenberg_menu' );
+			remove_action( 'admin_init', 'gutenberg_redirect_demo' );
+
+			remove_filter( 'wp_refresh_nonces', 'gutenberg_add_rest_nonce_to_heartbeat_response_headers' );
+			remove_filter( 'get_edit_post_link', 'gutenberg_revisions_link_to_editor' );
+			remove_filter( 'wp_prepare_revision_for_js', 'gutenberg_revisions_restore' );
+
+			remove_action( 'rest_api_init', 'gutenberg_register_rest_routes' );
+			remove_action( 'rest_api_init', 'gutenberg_add_taxonomy_visibility_field' );
+			remove_filter( 'rest_request_after_callbacks', 'gutenberg_filter_oembed_result' );
+			remove_filter( 'registered_post_type', 'gutenberg_register_post_prepare_functions' );
+
+			remove_action( 'do_meta_boxes', 'gutenberg_meta_box_save', 1000 );
+			remove_action( 'submitpost_box', 'gutenberg_intercept_meta_box_render' );
+			remove_action( 'submitpage_box', 'gutenberg_intercept_meta_box_render' );
+			remove_action( 'edit_page_form', 'gutenberg_intercept_meta_box_render' );
+			remove_action( 'edit_form_advanced', 'gutenberg_intercept_meta_box_render' );
+			remove_filter( 'redirect_post_location', 'gutenberg_meta_box_save_redirect' );
+			remove_filter( 'filter_gutenberg_meta_boxes', 'gutenberg_filter_meta_boxes' );
+
+			remove_action( 'admin_notices', 'gutenberg_build_files_notice' );
+			remove_filter( 'body_class', 'gutenberg_add_responsive_body_class' );
+			remove_filter( 'admin_url', 'gutenberg_modify_add_new_button_url' ); // old
+			remove_action( 'admin_enqueue_scripts', 'gutenberg_check_if_classic_needs_warning_about_blocks' );
+			remove_filter( 'register_post_type_args', 'gutenberg_filter_post_type_labels' );
+
+			remove_action( 'admin_init', 'gutenberg_add_edit_link_filters' );
+			remove_action( 'admin_print_scripts-edit.php', 'gutenberg_replace_default_add_new_button' );
+			remove_filter( 'redirect_post_location', 'gutenberg_redirect_to_classic_editor_when_saving_posts' );
+			remove_filter( 'display_post_states', 'gutenberg_add_gutenberg_post_state' );
+			remove_action( 'edit_form_top', 'gutenberg_remember_classic_editor_when_saving_posts' );
 		}
 	}
 
