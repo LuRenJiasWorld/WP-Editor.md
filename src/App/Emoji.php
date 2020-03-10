fa5852af7d25fbbc06face769b6359bd76bca8f9
Benjamin Chris <loli@lurenjia.in>
2020-03-10 12:30:59 +0800

    【Style】格式化App/Emoji.php代码样式

--- src/App/Emoji.php
+++ src/App/Emoji.php
@@ -2,19 +2,18 @@
 
 namespace EditormdApp;
 
-
 class Emoji {
 
 	public function __construct() {
 
-		add_action('wp_enqueue_scripts', array($this, 'emoji_enqueue_scripts'));
+		add_action("wp_enqueue_scripts", array($this, "emoji_enqueue_scripts"));
 
-        if( !in_array($GLOBALS['pagenow'], array('wp-login.php', 'wp-register.php')) ) {
-	        add_action('wp_print_footer_scripts', array($this, 'emoji_enqueue_footer_js'));
+        if(!in_array($GLOBALS["pagenow"], array("wp-login.php", "wp-register.php"))) {
+	        add_action("wp_print_footer_scripts", array($this, "emoji_enqueue_footer_js"));
         }
 
 		//禁用WordPress自带Emoji表情 ==> 排除干扰
-		add_action( 'init', array($this, 'disable_emojis') );
+		add_action("init", array($this, "disable_emojis"));
 	}
 
 	/**
@@ -22,8 +21,8 @@ class Emoji {
 	 */
 	public function emoji_enqueue_scripts() {
 
-		wp_enqueue_style( 'Emojify.js', $this->get_option('editor_addres','editor_style') . '/assets/Emojify.js/css/basic/emojify.min.css', array(), WP_EDITORMD_VER, 'all' );
-		wp_enqueue_script( 'Emojify.js', $this->get_option('editor_addres','editor_style') . '/assets/Emojify.js/js/emojify.min.js', array(), WP_EDITORMD_VER, true );
+		wp_enqueue_style("Emojify.js", $this->get_option("editor_addres","editor_style") . "/assets/Emojify.js/css/basic/emojify.min.css", array(), WP_EDITORMD_VER, "all");
+		wp_enqueue_script("Emojify.js", $this->get_option("editor_addres","editor_style") . "/assets/Emojify.js/js/emojify.min.js", array(), WP_EDITORMD_VER, true);
 	}
 
 	public function emoji_enqueue_footer_js() {
@@ -33,9 +32,9 @@ class Emoji {
                 emojify.setConfig({
                     img_dir: "<?php echo $this->get_option('editor_addres','editor_style') . '/assets/Emojify.js/images/basic' ?>",//前端emoji资源地址
                     blacklist: {
-                        'ids': [],
-                        'classes': ['no-emojify'],
-                        'elements': ['^script$', '^textarea$', '^pre$', '^code$']
+                        "ids": [],
+                        "classes": ["no-emojify"],
+                        "elements": ["^script$", "^textarea$", "^pre$", "^code$"]
                     }
                 });
                 emojify.run();
@@ -45,18 +44,18 @@ class Emoji {
 	}
 
 	/**
-	 * Disable the emoji's
+	 * Disable the emoji"s
 	 */
 	public function disable_emojis() {
-		remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-		remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-		remove_action( 'wp_print_styles', 'print_emoji_styles' );
-		remove_action( 'admin_print_styles', 'print_emoji_styles' );
-		remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-		remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
-		remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-		add_filter( 'tiny_mce_plugins', array($this, 'disable_emojis_tinymce') );
-		add_filter( 'wp_resource_hints', array($this, 'disable_emojis_remove_dns_prefetch'), 10, 2 );
+		remove_action("wp_head", "print_emoji_detection_script", 7);
+		remove_action("admin_print_scripts", "print_emoji_detection_script");
+		remove_action("wp_print_styles", "print_emoji_styles");
+		remove_action("admin_print_styles", "print_emoji_styles");
+		remove_filter("the_content_feed", "wp_staticize_emoji");
+		remove_filter("comment_text_rss", "wp_staticize_emoji");
+		remove_filter("wp_mail", "wp_staticize_emoji_for_email");
+		add_filter("tiny_mce_plugins", array($this, "disable_emojis_tinymce"));
+		add_filter("wp_resource_hints", array($this, "disable_emojis_remove_dns_prefetch"), 10, 2);
 	}
 
 	/**
@@ -65,9 +64,9 @@ class Emoji {
 	 * @param    array  $plugins
 	 * @return   array             Difference betwen the two arrays
 	 */
-	public function disable_emojis_tinymce( $plugins ) {
-		if ( is_array( $plugins ) ) {
-			return array_diff( $plugins, array( 'wpemoji' ) );
+	public function disable_emojis_tinymce($plugins) {
+		if (is_array($plugins)) {
+			return array_diff($plugins, array("wpemoji"));
 		} else {
 			return array();
 		}
@@ -80,15 +79,15 @@ class Emoji {
 	 * @param  string $relation_type The relation type the URLs are printed for.
 	 * @return array                 Difference betwen the two arrays.
 	 */
-	public function disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
+	public function disable_emojis_remove_dns_prefetch($urls, $relation_type) {
 
-		if ( 'dns-prefetch' == $relation_type ) {
+		if ("dns-prefetch" == $relation_type) {
 
 			// Strip out any URLs referencing the WordPress.org emoji location
-			$emoji_svg_url_bit = 'https://s.w.org/images/core/emoji/';
-			foreach ( $urls as $key => $url ) {
-				if ( strpos( $url, $emoji_svg_url_bit ) !== false ) {
-					unset( $urls[$key] );
+			$emoji_svg_url_bit = "https://s.w.org/images/core/emoji/";
+			foreach ($urls as $key => $url) {
+				if (strpos($url, $emoji_svg_url_bit) !== false) {
+					unset($urls[$key]);
 				}
 			}
 
@@ -106,11 +105,11 @@ class Emoji {
 	 *
 	 * @return mixed
 	 */
-	public function get_option( $option, $section, $default = '' ) {
+	public function get_option($option, $section, $default = "") {
 
-		$options = get_option( $section );
+		$options = get_option($section);
 
-		if ( isset( $options[ $option ] ) ) {
+		if (isset($options[ $option ])) {
 			return $options[ $option ];
 		}
 
