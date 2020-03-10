1d534feefc394f8140d256ca46d6fea09ebc0582
Benjamin Chris <loli@lurenjia.in>
2020-03-10 11:54:50 +0800

    【Feat】实现Config类，用于集中对配置的修改操作

---
+++ src/Utils/Config.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace EditormdUtils;
+
+// Config类用于优化对wp_option表的增删改查
+// 接受静态调用，无需实例化，减少各模块代码量
+class Config {
+    /**
+     * 获取字段值
+     *
+     * @param string $option  字段名称
+     * @param string $section 字段名称分组
+     * @param string $default 没搜索到返回空
+     *
+     * @return mixed
+     */
+    public static function get_option($option, $section, $default = '') {
+
+        $options = get_option($section);
+
+        if (isset($options[$option])) {
+            return $options[$option];
+        }
+
+        return $default;
+    }
+}
