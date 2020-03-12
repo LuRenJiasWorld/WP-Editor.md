0475fe7240621df2538e2f4ba57311a02305cfb5
Benjamin Chris <loli@lurenjia.in>
2020-03-12 19:36:27 +0800

    【Style】格式化Utils/Activator.php代码样式

--- src/Utils/Activator.php
+++ src/Utils/Activator.php
@@ -5,86 +5,84 @@ namespace EditormdUtils;
 class Activator {
 
 	public static function activate() {
-
 		// 初次载入插件写入默认数据 => 判断本地是否存在数据 不存在写入数据即可
-		if ( get_option( 'editor_basics' ) == false ) {
-			add_option( 'editor_basics', Activator::$defaultOptionsBasics, '', 'yes' );
+		if (get_option("editor_basics") == false) {
+			add_option("editor_basics", Activator::$defaultOptionsBasics, "", "yes");
 		}
 
-		if ( get_option( 'editor_style' ) == false ) {
-			add_option( 'editor_style', Activator::$defaultOptionsStyle, '', 'yes' );
+		if (get_option("editor_style") == false) {
+			add_option("editor_style", Activator::$defaultOptionsStyle, "", "yes");
 		}
 
-		if ( get_option( 'syntax_highlighting' ) == false ) {
-			add_option( 'syntax_highlighting', Activator::$defaultOptionsSyntax, '', 'yes' );
+		if (get_option("syntax_highlighting") == false) {
+			add_option("syntax_highlighting", Activator::$defaultOptionsSyntax, "", "yes");
 		}
 
-		if ( get_option( 'editor_emoji' ) == false ) {
-			add_option( 'editor_emoji', Activator::$defaultOptionsEmoji, '', 'yes' );
+		if (get_option("editor_emoji") == false) {
+			add_option("editor_emoji", Activator::$defaultOptionsEmoji, "", "yes");
 		}
 
-		if ( get_option( 'editor_toc' ) == false ) {
-			add_option( 'editor_toc', Activator::$defaultOptionsToc, '', 'yes' );
+		if (get_option("editor_toc") == false) {
+			add_option("editor_toc", Activator::$defaultOptionsToc, "", "yes");
 		}
 
-		if ( get_option( 'editor_latex' ) == false ) {
-			add_option( 'editor_latex', Activator::$defaultOptionsKatex, '', 'yes' );
+		if (get_option("editor_latex") == false) {
+			add_option("editor_latex", Activator::$defaultOptionsKatex, "", "yes");
 		}
 
-		if ( get_option( 'editor_mermaid' ) == false ) {
-			add_option( 'editor_mermaid', Activator::$defaultOptionsKatex, '', 'yes' );
+		if (get_option("editor_mermaid") == false) {
+			add_option("editor_mermaid", Activator::$defaultOptionsKatex, "", "yes");
 		}
 
-		if ( get_option( 'editor_mindmap' ) == false ) {
-			add_option( 'editor_mindmap', Activator::$defaultOptionsMindMap, '', 'yes' );
+		if (get_option("editor_mindmap") == false) {
+			add_option("editor_mindmap", Activator::$defaultOptionsMindMap, "", "yes");
 		}
 
-		if ( get_option( 'editor_advanced' ) == false ) {
-			add_option( 'editor_advanced', Activator::$defaultOptionsAdvanced, '', 'yes' );
+		if (get_option("editor_advanced") == false) {
+			add_option("editor_advanced", Activator::$defaultOptionsAdvanced, "", "yes");
 		}
-
 	}
 
 	public static $defaultOptionsBasics = array(
-		'task_list'      => 'off',
-		'imagepaste'     => 'off',
-		'live_preview'   => 'off',
-		'sync_scrolling' => 'off',
-		'html_decode'    => 'off',
-		'support_front'  => 'off',
-		'support_reply'  => 'off',
-		'support_other_text' => '',
-		'open_in_new_tab'=> 'off'
+		"task_list"      => "off",
+		"imagepaste"     => "off",
+		"live_preview"   => "off",
+		"sync_scrolling" => "off",
+		"html_decode"    => "off",
+		"support_front"  => "off",
+		"support_reply"  => "off",
+		"support_other_text" => "",
+		"open_in_new_tab"=> "off"
 	);
 
 	public static $defaultOptionsStyle = array(
-		'theme_style' => 'default',
-		'code_style'  => 'default',
-		'editor_addres' => ''
+		"theme_style" => "default",
+		"code_style"  => "default",
+		"editor_addres" => ""
 	);
 
 	public static $defaultOptionsSyntax = array(
-		'highlight_mode_auto'            => 'off',
-		'line_numbers'                   => 'off',
-		'show_language'                  => 'off',
-		'copy_clipboard'                 => 'off',
-		'highlight_library_style'        => 'default',
-		'customize_my_style'             => 'nothing',
-		'highlight_mode_customize'       => 'off',
-		'customize_highlight_style'      => 'nothing',
-		'customize_highlight_javascript' => 'nothing'
+		"highlight_mode_auto"            => "off",
+		"line_numbers"                   => "off",
+		"show_language"                  => "off",
+		"copy_clipboard"                 => "off",
+		"highlight_library_style"        => "default",
+		"customize_my_style"             => "nothing",
+		"highlight_mode_customize"       => "off",
+		"customize_highlight_style"      => "nothing",
+		"customize_highlight_javascript" => "nothing"
 	);
 
 	public static $defaultOptionsEmoji = array(
-		'support_emoji' => 'off'
+		"support_emoji" => "off"
 	);
 
 	public static $defaultOptionsToc = array(
-		'support_toc' => 'off'
+		"support_toc" => "off"
 	);
 
 	public static $defaultOptionsKatex = array(
-		'support_latex' => 'disable'
+		"support_latex" => "disable"
 	);
 
 	public static $defaultOptionsMermaid = array(
@@ -130,12 +128,12 @@ class Activator {
 	);
 
 	public static $defaultOptionsMindMap = array(
-		'support_mindmap' => 'off',
-		'customize_mindmap' => ''
+		"support_mindmap" => "off",
+		"customize_mindmap" => ""
 	);
 
 	public static $defaultOptionsAdvanced = array(
-		'jquery_compatible' => 'off',
-		'hide_ads' => 'off',
+		"jquery_compatible" => "off",
+		"hide_ads" => "off",
 	);
 }
