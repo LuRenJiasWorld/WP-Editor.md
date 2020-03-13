547a199915e8280edf12229cfd246171c9325aab
Benjamin Chris <loli@lurenjia.in>
2020-03-13 20:25:01 +0800

    【Style】格式化Utils/Deactivator.php代码样式

--- src/Utils/Deactivator.php
+++ src/Utils/Deactivator.php
@@ -3,10 +3,8 @@
 namespace EditormdUtils;
 
 class Deactivator {
-
 	public static function deactivate() {
 		//删除欢迎页提醒数据字段
-		delete_user_option( get_current_user_id(), 'dismissed_wp_pointers', true );
+		delete_user_option(get_current_user_id(), "dismissed_wp_pointers", true);
 	}
-
 }
