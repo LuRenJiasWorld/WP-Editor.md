b683d3ecdad95ec785bb5daefb29c686338d2858
Benjamin Chris <loli@lurenjia.in>
2020-03-23 18:26:00 +0800

    【Style】格式化assets/Config/editormd.js代码样式

--- assets/Config/editormd.js
+++ assets/Config/editormd.js
@@ -4,124 +4,124 @@
 (function ($, doc, win, editor) {
     $(doc).ready(function () {
         var textareaID = null;
-        if (doc.getElementById('wp-content-editor-container')) {
-            textareaID = 'wp-content-editor-container';
-        } else if (doc.getElementById('wp-replycontent-editor-container') && editor.supportReply === 'on') {
-            textareaID = 'wp-replycontent-editor-container';
-        } else if (doc.getElementById('comment') && editor.supportComment === 'on') {
-            textareaID = 'comment';
-            $('#comment').after("<div id='comment'></div>").remove();
+        if (doc.getElementById("wp-content-editor-container")) {
+            textareaID = "wp-content-editor-container";
+        } else if (doc.getElementById("wp-replycontent-editor-container") && editor.supportReply === "on") {
+            textareaID = "wp-replycontent-editor-container";
+        } else if (doc.getElementById("comment") && editor.supportComment === "on") {
+            textareaID = "comment";
+            $("#comment").after('<div id="comment"></div>').remove();
         } else if (editor.supportOther !== '') {
             // 自定义ID编辑器
             textareaID = editor.supportOther;
-            $('#' + editor.supportOther).after("<div id=" + editor.supportOther + "></div>").remove();
+            $("#" + editor.supportOther).after('<div id="' + editor.supportOther + '"></div>').remove();
         } else {
             return false;
         }
 
         //完整菜单
         var fullToolBar = [
-            'undo', 'redo', '|',
-            'bold', 'del', 'italic', 'quote', 'ucwords', 'uppercase', 'lowercase', '|',
-            'h1', 'h2', 'h3', 'h4', 'h5', 'h6', '|',
-            'list-ul', 'list-ol', 'hr', '|',
-            'link', 'reference-link', 'image', 'code', 'code-block', 'table', 'datetime', editor.emoji !== 'off' ? 'emoji' : '' + 'html-entities', 'more', 'pagebreak', '|',
-            'goto-line', 'watch', 'preview', 'fullscreen', 'clear', 'search', '|',
-            'help', 'info'
+            "undo", "redo", "|",
+            "bold", "del", "italic", "quote", "ucwords", "uppercase", "lowercase", "|",
+            "h1", "h2", "h3", "h4", "h5", "h6", "|",
+            "list-ul", "list-ol", "hr", "|",
+            "link", "reference-link", "image", "code", "code-block", "table", "datetime", editor.emoji !== "off" ? "emoji" : "" + "html-entities", "more", "pagebreak", "|",
+            "goto-line", "watch", "preview", "fullscreen", "clear", "search", "|",
+            "help", "info"
         ];
         var simpleToolBar = [
-            'bold', 'del', 'italic', 'quote', 'ucwords', 'uppercase', 'lowercase', '|',
-            'link', 'reference-link', 'image', 'code', 'code-block', 'table', 'datetime', editor.emoji !== 'off' ? 'emoji' : '' + 'html-entities', '|',
-            'watch', 'preview', 'fullscreen', 'clear', 'info'
+            "bold", "del", "italic", "quote", "ucwords", "uppercase", "lowercase", "|",
+            "link", "reference-link", "image", "code", "code-block", "table", "datetime", editor.emoji !== "off" ? "emoji" : "" + "html-entities", "|",
+            "watch", "preview", "fullscreen", "clear", "info"
         ];
         var miniToolBar = [
-            'ucwords', 'uppercase', 'lowercase', '|',
-            'link', 'reference-link', 'image', 'table', 'datetime', editor.emoji !== 'off' ? 'emoji' : '' + 'html-entities', '|',
-            'watch', 'preview', 'fullscreen', 'info'
+            "ucwords", "uppercase", "lowercase", "|",
+            "link", "reference-link", "image", "table", "datetime", editor.emoji !== "off" ? "emoji" : "" + "html-entities", "|",
+            "watch", "preview", "fullscreen", "info"
         ];
 
         var toolBar;
         switch (textareaID) {
-            case 'wp-content-editor-container' :
+            case "wp-content-editor-container":
                 toolBar = fullToolBar;
                 break;
-            case 'comment' :
+            case "comment":
                 toolBar = simpleToolBar;
                 break;
-            case 'wp-replycontent-editor-container' :
+            case "wp-replycontent-editor-container":
                 toolBar = miniToolBar;
                 break;
-            default :
+            default:
                 toolBar = fullToolBar;
                 break;
         }
 
         var wpEditormd = editormd({
             id: textareaID,
-            path: editor.editormdUrl + '/assets/Editormd/lib/',
-            width: '100%', //编辑器宽度
-            height: textareaID === 'wp-content-editor-container' ? 640 : 320,    //编辑器高度
-            syncScrolling: editor.syncScrolling !== 'off', //即是否开启同步滚动预览
-            watch: textareaID === 'wp-replycontent-editor-container' ? false : editor.livePreview !== 'off', //即是否开启实时预览
-            htmlDecode: editor.htmlDecode !== 'off', //HTML标签解析
+            path: editor.editormdUrl + "/assets/Editormd/lib/",
+            width: "100%", //编辑器宽度
+            height: textareaID === "wp-content-editor-container" ? 640 : 320,    //编辑器高度
+            syncScrolling: editor.syncScrolling !== "off", //即是否开启同步滚动预览
+            watch: textareaID === "wp-replycontent-editor-container" ? false : editor.livePreview !== "off", //即是否开启实时预览
+            htmlDecode: editor.htmlDecode !== "off", //HTML标签解析
             toolbarAutoFixed: false, //工具栏是否自动固定
             toolbar: true,
-            autoFocus: textareaID !== 'comment', //判断场景是否跳转到编辑器区域
+            autoFocus: textareaID !== "comment", //判断场景是否跳转到编辑器区域
             tocm: false, //同TOC 不过不合适
-            tocContainer: editor.toc === 'off' ? false : '', //TOC
+            tocContainer: editor.toc === "off" ? false : "", //TOC
             tocDropdown: false, //下拉TOC
             theme: editor.theme, //编辑器总体主题
             previewTheme: editor.previewTheme, //编辑器主题
             editorTheme: editor.editorTheme, //编辑器主题
-            emoji: editor.emoji !== 'off', //Emoji表情
-            tex: editor.tex === 'katex', //LaTeX公式
-            mind: editor.mindMap !== 'off', //思维导图
-            mermaid: editor.mermaid !== 'off', //Mermaid
+            emoji: editor.emoji !== "off", //Emoji表情
+            tex: editor.tex === "katex", //LaTeX公式
+            mind: editor.mindMap !== "off", //思维导图
+            mermaid: editor.mermaid !== "off", //Mermaid
             atLink: false,//Github @Link
-            taskList: editor.taskList !== 'off',//task lists
-            imageFormats: ['jpg', 'jpeg', 'gif', 'png', 'bmp', 'webp'],
+            taskList: editor.taskList !=="off",//task lists
+            imageFormats: ["jpg", "jpeg", "gif", "png", "bmp", "webp"],
             placeholder: editor.placeholderEditor, //编辑器placeholder
             prismTheme: editor.prismTheme, //Prism主題风格
-            prismLineNumbers: editor.prismLineNumbers !== 'off',
+            prismLineNumbers: editor.prismLineNumbers !== "off",
             saveHTMLToTextarea: true,
             toolbarIcons: function () {
                 return toolBar;
             },
             //强制全屏
             onfullscreen: function () {
-                doc.getElementById(textareaID).style.position = 'fixed';
-                doc.getElementById(textareaID).style.zIndex = '99999';
+                doc.getElementById(textareaID).style.position = "fixed";
+                doc.getElementById(textareaID).style.zIndex = "99999";
             },
             //退出全屏返回原来的样式
             onfullscreenExit: function () {
-                doc.getElementById(textareaID).style.position = 'relative';
-                doc.getElementById(textareaID).style.zIndex = 'auto';
+                doc.getElementById(textareaID).style.position = "relative";
+                doc.getElementById(textareaID).style.zIndex = "auto";
             },
             onload: function () {
                 //加载完成执行
-                if (textareaID === 'comment') {
+                if (textareaID === "comment") {
                     //修改评论表单name
-                    $('textarea.editormd-markdown-textarea').attr('name', 'comment');
+                    $("textarea.editormd-markdown-textarea").attr("name", "comment");
                 }
 
-                if (textareaID === 'wp-replycontent-editor-container') {
-                    $('.reply').click(function () {
+                if (textareaID === "wp-replycontent-editor-container") {
+                    $(".reply").click(function () {
                         setTimeout(function () {
-                            $('.edit-comments-php .CodeMirror.cm-s-default.CodeMirror-wrap').css('margin-top', $('.editormd-toolbar').height());
+                            $(".edit-comments-php .CodeMirror.cm-s-default.CodeMirror-wrap").css("margin-top", $(".editormd-toolbar").height());
                         }, 100);
                     })
                 }
 
                 if (getWidth() === 1600) {
                     // 1600分辨率删除编辑器编辑空白外边距
-                    var codeMirror = $('.editormd .CodeMirror.CodeMirror-wrap');
-                    var codeMirrorMarginTop = codeMirror.css('margin-top');
-                    codeMirror.css('margin-top', parseInt(codeMirrorMarginTop) - 32 + "px");
+                    var codeMirror = $(".editormd .CodeMirror.CodeMirror-wrap");
+                    var codeMirrorMarginTop = codeMirror.css("margin-top");
+                    codeMirror.css("margin-top", parseInt(codeMirrorMarginTop) - 32 + "px");
                 }
             }
         });
         // WP Media module支持
-        if (typeof wp !== 'undefined' && typeof wp.media !== 'undefined') {
+        if (typeof wp !== "undefined" && typeof wp.media !== "undefined") {
             var original_wp_media_editor_insert = wp.media.editor.insert;
             wp.media.editor.insert = function (html) {
                 // 显示Loading画面，避免用户以为添加失败
@@ -149,7 +149,7 @@
         var updateWordCounter = setInterval(function() {
             // wp.utils.WordCounter()在前台评论部分不存在，因此需要判断一下，避免出现错误
             if (wp.utils) {
-                var $count = $( '#wp-word-count' ).find( '.word-count' );
+                var $count = $("#wp-word-count").find(".word-count");
                 var html = wpEditormd.getHTML();
 
                 var wordCounter = new wp.utils.WordCounter();
@@ -161,8 +161,8 @@
             }
         }, 1000);
         // 图像粘贴
-        if (editor.imagePaste === 'on') {
-            $('#' + textareaID).on('paste', function (event) {
+        if (editor.imagePaste === "on") {
+            $("#" + textareaID).on("paste", function (event) {
                 event = event.originalEvent;
                 var cbd = window.clipboardData || event.clipboardData; //兼容ie||chrome
                 var ua = window.navigator.userAgent;
@@ -174,8 +174,8 @@
                     line: wpEditormd.getCursor().line,
                     ch: wpEditormd.getCursor().ch
                 };
-                if (cbd.items && cbd.items.length === 2 && cbd.items[0].kind === 'string' && cbd.items[1].kind === 'file' &&
-                    cbd.types && cbd.types.length === 2 && cbd.types[0] === 'text/plain' && cbd.types[1] === 'Files' &&
+                if (cbd.items && cbd.items.length === 2 && cbd.items[0].kind === "string" && cbd.items[1].kind === "file" &&
+                    cbd.types && cbd.types.length === 2 && cbd.types[0] === "text/plain" && cbd.types[1] === "Files" &&
                     ua.match(/Macintosh/i) && Number(ua.match(/Chrome\/(\d{2})/i)[1]) < 49) {
                     return;
                 }
@@ -183,12 +183,12 @@
                 if (itemLength === 0) {
                     return;
                 }
-                if (itemLength === 1 && cbd.items[0].kind === 'string') {
+                if (itemLength === 1 && cbd.items[0].kind === "string") {
                     return;
                 }
 
                 // 此处itemLength等于2的情况是为了兼容MacOS，其剪贴板内包含两个元素，一个是文件名，一个是文件二进制数据
-                if ((itemLength === 1 && cbd.items[0].kind === 'file') || itemLength === 2 && cbd.items[1].kind === 'file') {        
+                if ((itemLength === 1 && cbd.items[0].kind === "file") || itemLength === 2 && cbd.items[1].kind === "file") {        
                     if (itemLength === 1) {
                         var item = cbd.items[0];
                     } else {
@@ -213,11 +213,11 @@
                     //Base64转Blob对象
                     function dataURItoBlob(base64Data) {
                         var byteString;
-                        if (base64Data.split(',')[0].indexOf('base64') >= 0)
-                            byteString = atob(base64Data.split(',')[1]);
+                        if (base64Data.split(",")[0].indexOf("base64") >= 0)
+                            byteString = atob(base64Data.split(",")[1]);
                         else
-                            byteString = decodeURI(base64Data.split(',')[1]);
-                        var mimeString = base64Data.split(',')[0].split(':')[1].split(';')[0];
+                            byteString = decodeURI(base64Data.split(",")[1]);
+                        var mimeString = base64Data.split(",")[0].split(":")[1].split(";")[0];
                         var ia = new Uint8Array(byteString.length);
                         for (var i = 0; i < byteString.length; i++) {
                             ia[i] = byteString.charCodeAt(i);
@@ -228,27 +228,27 @@
 
                     //传参
                     readBlobAsDataURL(blob, function (dataurl) {
-                        var uploadingText = '![' + editor.imgUploading + ']';
-                        var uploadFailText = '![' + editor.imgUploadeFailed + ']';
+                        var uploadingText = "![" + editor.imgUploading + "]";
+                        var uploadFailText = "![" + editor.imgUploadeFailed + "]";
                         var data = {
-                            action: 'imagepaste_action',
+                            action: "imagepaste_action",
                             dataurl: dataurl
                         };
                         wpEditormd.insertValue(uploadingText);
 
                         $.ajax({
                             url: ajaxurl,
-                            type: 'POST',
+                            type: "POST",
                             data: data,
                             success: function (request) {
                                 var obj = JSON.parse(request);
                                 if (obj.error) {
                                     wpEditormd.setValue(wpEditormd.getValue().replace(uploadingText, uploadFailText));
                                 } else {
-                                    if ( editor.imageLink === 'on' ) {
-                                        wpEditormd.setValue(wpEditormd.getValue().replace(uploadingText, '[' + '![](' + obj.url + ')' + '](' + obj.url + ')'));
+                                    if ( editor.imageLink === "on" ) {
+                                        wpEditormd.setValue(wpEditormd.getValue().replace(uploadingText, "[" + "![](" + obj.url + ")" + "](" + obj.url + ")"));
                                     } else {
-                                        wpEditormd.setValue(wpEditormd.getValue().replace(uploadingText, '![](' + obj.url + ')'));
+                                        wpEditormd.setValue(wpEditormd.getValue().replace(uploadingText, "![](" + obj.url + ")"));
                                     }
                                 }
                                 // 移动光标
@@ -263,38 +263,38 @@
     });
     // CodeMirror
     editormd.codeMirrorURL = {
-        url: cdn_url(editor.editormdUrl, 'codemirror')
+        url: cdn_url(editor.editormdUrl, "codemirror")
     };
     // Marked
     editormd.markedURL = {
-        js: cdn_url(editor.editormdUrl, 'marked') + '/marked.min'
+        js: cdn_url(editor.editormdUrl, "marked") + "/marked.min"
     };
     // Prism高亮库
     editormd.prismURL = {
-        url: cdn_url(editor.editormdUrl, 'prismjs')
+        url: cdn_url(editor.editormdUrl, "prismjs")
     };
 
     // 思维导图自定义地址
-    editormd.mindMapURL = splitFileName(editor.mindMapURL)
+    editormd.mindMapURL = splitFileName(editor.mindMapURL);
 
     // KaTeX科学公式配置
-    if (editor.tex === 'on') {
+    if (editor.tex === "on") {
         editormd.katexURL = {
-            css: cdn_url(editor.editormdUrl, 'katex') + '/katex.min',
-            js: cdn_url(editor.editormdUrl, 'katex') + '/katex.min'
-        }
+            css: cdn_url(editor.editormdUrl, "katex") + "/katex.min",
+            js: cdn_url(editor.editormdUrl, "katex") + "/katex.min"
+        };
     }
     // Mermaid配置
-    if (editor.mermaid === 'on') {
+    if (editor.mermaid === "on") {
         editormd.mermaidURL = {
-            js: cdn_url(editor.editormdUrl, 'mermaid') + '/mermaid.min'
-        }
+            js: cdn_url(editor.editormdUrl, "mermaid") + "/mermaid.min"
+        };
     }
     // Emoji表情配置
-    if (editor.emoji === 'on') {
+    if (editor.emoji === "on") {
         editormd.emoji = {
-            path: cdn_url(editor.editormdUrl, 'emojify') + '/',
-            ext: '.png'
+            path: cdn_url(editor.editormdUrl, "emojify") + "/",
+            ext: ".png"
         };
     }
 
@@ -307,23 +307,23 @@
     function cdn_url(url, lib) {
         var lib_url;
         switch (lib) {
-            case 'emojify':
-                lib_url = url + '/assets/Emojify.js/images/basic';
+            case "emojify":
+                lib_url = url + "/assets/Emojify.js/images/basic";
                 break;
-            case 'katex':
-                lib_url = url + '/assets/KaTeX';
+            case "katex":
+                lib_url = url + "/assets/KaTeX";
                 break;
-            case 'mermaid':
-                lib_url = url + '/assets/Mermaid';
+            case "mermaid":
+                lib_url = url + "/assets/Mermaid";
                 break;
-            case 'prismjs':
-                lib_url = url + '/assets/Prism.js';
+            case "prismjs":
+                lib_url = url + "/assets/Prism.js";
                 break;
-            case 'codemirror':
-                lib_url = url + '/assets/CodeMirror';
+            case "codemirror":
+                lib_url = url + "/assets/CodeMirror";
                 break;
-            case 'marked':
-                lib_url = url + '/assets/Marked'
+            case "marked":
+                lib_url = url + "/assets/Marked";
         }
         return lib_url;
     }
