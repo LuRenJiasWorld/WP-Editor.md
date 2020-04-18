fbaf5f6ab362c9f0e9ccaa62576ca0c7d9343959
Benjamin Chris <loli@lurenjia.in>
2020-04-18 19:43:30 +0800

    【Feat】新增webpack.config.dev.js

---
+++ webpack.config.dev.js
@@ -0,0 +1,44 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    "assets/Config/editormd": "./assets/Config/editormd.js",
+    "assets/FrontStyle/frontstyle": "./assets/FrontStyle/frontstyle.js",
+  },
+  output: {
+    filename: "[name].min.js",
+    path: path.resolve(__dirname),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss|sass)$/,
+        include: [path.resolve(__dirname, "assets/Config")],
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("dart-sass"),
+            },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: [require("autoprefixer")({})],
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
