f1eda7e9b1d167511a67c5d7e8fe0190560d48a2
Benjamin Chris <loli@lurenjia.in>
2020-04-23 19:48:09 +0800

    【Feat】webpack打包后的文件包含版权标识

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,5 +1,19 @@
 const path = require("path");
 const webpack = require("webpack");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+const date = new Date();
+const version = require("./package.json").version;
+
+const banner = `/*!
+ * WP Editor.md v${version} \
+| Author: @LuRenJiasWorld \
+| Repository: https://github.com/LuRenJiasWorld/WP-Editor.md \
+| Compiled on ${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")} \
+| License: GNU General Public License v3.0
+ * NOTICE: Code in assets/Editormd is originally developed by @pandao and forked to work with WP Editor.md by @LuRenJiasWorld. 
+ *         Related MIT license is located in assets/Editormd/LICENSE, but now distributed with WP Editor.md using GPLv3.
+ */`;
 
 module.exports = {
   mode: "production",
@@ -17,17 +31,34 @@ module.exports = {
     path: path.resolve(__dirname),
   },
   devtool: false,
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        sourceMap: true,
+        uglifyOptions: {
+          output: {
+            beautify: false,
+            preamble: banner,
+          },
+        },
+      }),
+    ],
+  },
   plugins: [
-    new webpack.SourceMapDevToolPlugin({})
+    new webpack.SourceMapDevToolPlugin({}),
+    new webpack.BannerPlugin({
+      banner: banner,
+      raw: true,
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.js$/,
-            use: {
-                loader: 'babel-loader'
-            },
-            exclude: '/node_modules/'
+        use: {
+          loader: "babel-loader",
+        },
+        exclude: "/node_modules/",
       },
       {
         test: /\.(css|scss|sass)$/,
