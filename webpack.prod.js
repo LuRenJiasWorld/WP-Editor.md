96548021488bfea31546226e2acea8a94e455d75
Benjamin Chris <loli@lurenjia.in>
2020-04-25 20:26:04 +0800

    【Feat】重新区分生产环境和开发环境的webpack配置文件，但只保留入口

---
+++ webpack.prod.js
@@ -0,0 +1,8 @@
+const merge = require('webpack-merge');
+const common = require("./webpack.common");
+
+process.env.NODE_ENV = "production";
+
+module.exports = merge(common, {
+	mode: "production"
+});
