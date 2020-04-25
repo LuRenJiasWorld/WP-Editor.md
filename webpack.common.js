b6d321097fa5060aafee596976e40edf73ee8d98
Benjamin Chris <loli@lurenjia.in>
2020-04-25 20:26:25 +0800

    【Feat】新增webpack.common.js对所有相关文件进行打包构建

--- webpack.common.js
+++ webpack.common.js
@@ -1,76 +1,184 @@
-const date = new Date();
-const version = require("./package.json").version;
+const path = require("path");
+const webpack = require("webpack");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MergeIntoSingleFilePlugin = require("webpack-merge-and-include-globally");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
 
-const Common = {
-	"banner": `/*!
-	* WP Editor.md v${version} \
-   | Author: @LuRenJiasWorld \
-   | Repository: https://github.com/LuRenJiasWorld/WP-Editor.md \
-   | Compiled on ${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")} \
-   | License: GNU General Public License v3.0
-	* NOTICE: Code in assets/Editormd is originally developed by @pandao and forked to work with WP Editor.md by @LuRenJiasWorld. 
-	*         Related MIT license is located in assets/Editormd/LICENSE, but now distributed with WP Editor.md using GPLv3.
-	*/`,
+const Conf = require("./webpack.conf");
 
-	Editormd: {
-		CodeMirror: {
-			modes: [
-				"assets/CodeMirror/mode/meta.js",
-				"assets/CodeMirror/mode/css/css.js",
-				"assets/CodeMirror/mode/sass/sass.js",
-				"assets/CodeMirror/mode/shell/shell.js",
-				"assets/CodeMirror/mode/sql/sql.js",
-				"assets/CodeMirror/mode/clike/clike.js",
-				"assets/CodeMirror/mode/php/php.js",
-				"assets/CodeMirror/mode/xml/xml.js",
-				"assets/CodeMirror/mode/markdown/markdown.js",
-				"assets/CodeMirror/mode/javascript/javascript.js",
-				"assets/CodeMirror/mode/htmlmixed/htmlmixed.js",
-				"assets/CodeMirror/mode/gfm/gfm.js",
-				"assets/CodeMirror/mode/http/http.js",
-				"assets/CodeMirror/mode/go/go.js",
-				"assets/CodeMirror/mode/dart/dart.js",
-				"assets/CodeMirror/mode/coffeescript/coffeescript.js",
-				"assets/CodeMirror/mode/nginx/nginx.js",
-				"assets/CodeMirror/mode/python/python.js",
-				"assets/CodeMirror/mode/perl/perl.js",
-				"assets/CodeMirror/mode/lua/lua.js",
-				"assets/CodeMirror/mode/r/r.js",
-				"assets/CodeMirror/mode/ruby/r.js",
-				"assets/CodeMirror/mode/rst/rst.js",
-				"assets/CodeMirror/mode/smartymixed/smartmixed.js",
-				"assets/CodeMirror/mode/vb/vb.js",
-				"assets/CodeMirror/mode/vbscript/vbscript.js",
-				"assets/CodeMirror/mode/velocity/velocity.js",
-				"assets/CodeMirror/mode/xquery/xquery.js",
-				"assets/CodeMirror/mode/yaml/yaml.js",
-				"assets/CodeMirror/mode/erlang/erlang.js",
-				"assets/CodeMirror/mode/jade/jade.js",
-			],
-			addons: [
-				"assets/CodeMirror/addon/edit/trailingspace.js",
-				"assets/CodeMirror/addon/dialog/dialog.js",
-				"assets/CodeMirror/addon/search/searchcursor.js",
-				"assets/CodeMirror/addon/search/search.js",
-				"assets/CodeMirror/addon/scroll/annotatescrollbar.js",
-				"assets/CodeMirror/addon/search/matchesonscrollbar.js",
-				"assets/CodeMirror/addon/display/placeholder.js",
-				"assets/CodeMirror/addon/edit/closetag.js",
-				"assets/CodeMirror/addon/fold/foldcode.js",
-				"assets/CodeMirror/addon/fold/foldgutter.js",
-				"assets/CodeMirror/addon/fold/indent-fold.js",
-				"assets/CodeMirror/addon/fold/brace-fold.js",
-				"assets/CodeMirror/addon/fold/xml-fold.js",
-				"assets/CodeMirror/addon/fold/markdown-fold.js",
-				"assets/CodeMirror/addon/fold/comment-fold.js",
-				"assets/CodeMirror/addon/mode/overlay.js",
-				"assets/CodeMirror/addon/selection/active-line.js",
-				"assets/CodeMirror/addon/edit/closebrackets.js",
-				"assets/CodeMirror/addon/display/fullscreen.js",
-				"assets/CodeMirror/addon/search/match-highlighte.js"
-			]
-		}
-	}
-}
+const banner = Conf.banner;
 
-module.exports = Common;
+const mode = process.env.NODE_ENV;
+const isDevMode = mode === "development";
+
+module.exports = {
+  mode: "",
+  performance: {
+    maxAssetSize: isDevMode ? 1024 * 1024 * 4 : 1024 * 512,
+    maxEntrypointSize: isDevMode ? 1024 * 1024 * 4 : 1024 * 512,
+  },
+  entry: {
+    "assets/Editormd/editormd": "./assets/Editormd/src/editormd.js",
+    "assets/Editormd/editormd.min.css": "./assets/Editormd/scss/editormd.scss",
+    "assets/Editormd/editormd.preview.min.css": "./assets/Editormd/scss/editormd.preview.scss",
+    "assets/Config/editormd": "./assets/Config/editormd.js",
+    "assets/FrontStyle/frontstyle": "./assets/FrontStyle/frontstyle.js",
+  },
+  output: {
+    filename: "[name].min.js",
+    path: path.resolve(__dirname),
+  },
+  devtool: false,
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        sourceMap: isDevMode ? true : false,
+        uglifyOptions: {
+          output: {
+            beautify: false,
+            preamble: banner,
+          },
+        },
+      }),
+    ],
+  },
+  plugins: [
+    new FixStyleOnlyEntriesPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name]",
+    }),
+    isDevMode
+      ? new webpack.SourceMapDevToolPlugin({
+          filename: "[name].min.js.map",
+          exclude: /\.css$/,
+        })
+      : null,
+    isDevMode
+      ? new webpack.SourceMapDevToolPlugin({
+          filename: "[name].map",
+          include: /\.css$/,
+        })
+      : null,
+    new webpack.BannerPlugin({
+      banner: banner,
+      raw: true,
+    }),
+    new MergeIntoSingleFilePlugin({
+      files: [
+        {
+          src: Conf.Editormd.CodeMirror.modes,
+          dest: function (code) {
+            if (isDevMode) {
+              const min = require("uglify-js").minify(code, {
+                sourceMap: {
+                  includeSources: false,
+                  filename: "assets/Editormd/lib/modes.min.js",
+                  url: "modes.min.js.map",
+                },
+              });
+              return {
+                "assets/Editormd/lib/modes.min.js": banner + min.code,
+                "assets/Editormd/lib/modes.min.js.map": min.map,
+              };
+            } else {
+              const min = require("uglify-js").minify(code);
+              return {
+                "assets/Editormd/lib/modes.min.js": banner + min.code,
+              };
+            }
+          },
+        },
+        {
+          src: Conf.Editormd.CodeMirror.addons,
+          dest: function (code) {
+            if (isDevMode) {
+              const min = require("uglify-js").minify(code, {
+                sourceMap: {
+                  includeSources: false,
+                  filename: "assets/Editormd/lib/addons.min.js",
+                  url: "addons.min.js.map",
+                },
+              });
+              return {
+                "assets/Editormd/lib/addons.min.js": banner + min.code,
+                "assets/Editormd/lib/addons.min.js.map": min.map,
+              };
+            } else {
+              const min = require("uglify-js").minify(code);
+              return {
+                "assets/Editormd/lib/addons.min.js": banner + min.code,
+              };
+            }
+          },
+        },
+      ],
+    }),
+  ].filter(Boolean),
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+        },
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.scss$/,
+        include: [path.resolve(__dirname, "assets/Editormd/scss")],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+            options: { url: false },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: [require("autoprefixer")({})],
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("node-sass"),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(css|scss|sass)$/,
+        include: [
+          path.resolve(__dirname, "assets/Config"),
+          path.resolve(__dirname, "assets/FrontStyle"),
+        ],
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: [require("autoprefixer")({})],
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("node-sass"),
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
