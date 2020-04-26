e028596d349e49a135219f07677feaf74836062d
Benjamin Chris <loli@lurenjia.in>
2020-04-26 20:08:06 +0800

    【Feat】新增性能分析插件BundleAnalyzerPlugin

--- webpack.common.js
+++ webpack.common.js
@@ -53,7 +53,18 @@ module.exports = {
     ],
   },
   plugins: [
-    new FixStyleOnlyEntriesPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "server",
+      analyzerHost: "127.0.0.1",
+      analyzerPort: 8889,
+      reportFilename: "report.html",
+      defaultSizes: "parsed",
+      openAnalyzer: true,
+      generateStatsFile: false,
+      statsFilename: "stats.json",
+      statsOptions: null,
+      logLevel: "info",
+    }),
     new MiniCssExtractPlugin({
       filename: "[name]",
     }),
