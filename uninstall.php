98eb316da2af1fbb2afcf54a8cb56132f5d912db
Benjamin Chris <loli@lurenjia.in>
2020-05-10 19:18:19 +0800

    【Style】格式化uninstall.php缩进为四个空格

--- uninstall.php
+++ uninstall.php
@@ -1,33 +1,33 @@
 <?php
 
 if (
-	! defined( 'WP_UNINSTALL_PLUGIN' )
-	||
-	! WP_UNINSTALL_PLUGIN
-	||
-	dirname( WP_UNINSTALL_PLUGIN ) != dirname( plugin_basename( __FILE__ ) )
+    ! defined( 'WP_UNINSTALL_PLUGIN' )
+    ||
+    ! WP_UNINSTALL_PLUGIN
+    ||
+    dirname( WP_UNINSTALL_PLUGIN ) != dirname( plugin_basename( __FILE__ ) )
 ) {
-	status_header( 404 );
-	exit;
+    status_header( 404 );
+    exit;
 }
 
 static $options_name = array(
-	'editor_basics',
-	'editor_style',
-	'syntax_highlighting',
-	'editor_emoji',
-	'editor_toc',
-	'editor_latex',
-	'editor_mermaid',
-	'editor_mindmap',
-	'editor_advanced',
-	'editor_version'
+    'editor_basics',
+    'editor_style',
+    'syntax_highlighting',
+    'editor_emoji',
+    'editor_toc',
+    'editor_latex',
+    'editor_mermaid',
+    'editor_mindmap',
+    'editor_advanced',
+    'editor_version'
 );
 
 
 // 删除选项
 foreach($options_name as $optionName) {
-	delete_option($optionName);
+    delete_option($optionName);
 }
 
 //开启自带可视化编辑器
