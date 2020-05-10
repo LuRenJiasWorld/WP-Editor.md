7d2986295d6daf48f478056415ee82cbd4610a7a
Benjamin Chris <loli@lurenjia.in>
2020-05-10 19:07:56 +0800

    【Style】格式化src/Front下所有文件缩进为两个空格

--- src/Front/Controller.php
+++ src/Front/Controller.php
@@ -3,136 +3,136 @@
 namespace EditormdFront;
 
 class Controller {
-	/**
-	 * @var string 插件名称
-	 */
-	private $plugin_name;
+    /**
+     * @var string 插件名称
+     */
+    private $plugin_name;
 
-	/**
-	 * @var string 插件版本号
-	 */
-	private $version;
+    /**
+     * @var string 插件版本号
+     */
+    private $version;
 
-	/**
-	 * @var string 翻译文本域
-	 */
-	private $text_domain;
+    /**
+     * @var string 翻译文本域
+     */
+    private $text_domain;
 
-	/**
-	 * @var string 静态资源地址
-	 */
-	private $front_static_url;
+    /**
+     * @var string 静态资源地址
+     */
+    private $front_static_url;
 
-	/**
-	 * Controller constructor 初始化类并设置其属性
-	 *
-	 * @param $plugin_name
-	 * @param $version
-	 * @param $ioption
-	 */
-	public function __construct() {
-		$this->plugin_name      = "WP Editor.md";
-		$this->text_domain      = "editormd";
-		$this->version          = WP_EDITORMD_VER;
-		$this->front_static_url = $this->get_option("editor_addres", "editor_style");
+    /**
+     * Controller constructor 初始化类并设置其属性
+     *
+     * @param $plugin_name
+     * @param $version
+     * @param $ioption
+     */
+    public function __construct() {
+        $this->plugin_name      = "WP Editor.md";
+        $this->text_domain      = "editormd";
+        $this->version          = WP_EDITORMD_VER;
+        $this->front_static_url = $this->get_option("editor_addres", "editor_style");
 
-		add_action("wp_enqueue_scripts", array($this, "enqueue_front_styles"));
-		add_action("wp_enqueue_scripts", array($this, "enqueue_front_scripts"));
-	}
+        add_action("wp_enqueue_scripts", array($this, "enqueue_front_styles"));
+        add_action("wp_enqueue_scripts", array($this, "enqueue_front_scripts"));
+    }
 
-	/**
-	 * 注册样式文件
-	 */
-	public function enqueue_front_styles() {
-		//Style - Editor.md
-		wp_enqueue_style("Editormd_Front", $this->front_static_url . "/assets/Editormd/editormd.min.css", array(), WP_EDITORMD_VER, "all");
-	}
+    /**
+     * 注册样式文件
+     */
+    public function enqueue_front_styles() {
+        //Style - Editor.md
+        wp_enqueue_style("Editormd_Front", $this->front_static_url . "/assets/Editormd/editormd.min.css", array(), WP_EDITORMD_VER, "all");
+    }
 
-	/**
-	 * 注册脚本文件
-	 */
-	public function enqueue_front_scripts() {
+    /**
+     * 注册脚本文件
+     */
+    public function enqueue_front_scripts() {
 
-		//兼容模式 - jQuery
-		if ($this->get_option("jquery_compatible", "editor_advanced") !== "off") {
-			wp_enqueue_script("jquery", null, null, array(), false);
-			wp_enqueue_script("Editormd_Front", $this->front_static_url . "/assets/Editormd/editormd.min.js", array("jquery"), WP_EDITORMD_VER, true);
-		} else {
-			wp_deregister_script("jquery");
-			wp_enqueue_script("jQuery-CDN", $this->front_static_url . "/assets/jQuery/jquery.min.js", array(), "1.12.4", true);
-			wp_enqueue_script("Editormd_Front", $this->front_static_url . "/assets/Editormd/editormd.min.js", array("jQuery-CDN"), WP_EDITORMD_VER, true);
-		}
+        //兼容模式 - jQuery
+        if ($this->get_option("jquery_compatible", "editor_advanced") !== "off") {
+            wp_enqueue_script("jquery", null, null, array(), false);
+            wp_enqueue_script("Editormd_Front", $this->front_static_url . "/assets/Editormd/editormd.min.js", array("jquery"), WP_EDITORMD_VER, true);
+        } else {
+            wp_deregister_script("jquery");
+            wp_enqueue_script("jQuery-CDN", $this->front_static_url . "/assets/jQuery/jquery.min.js", array(), "1.12.4", true);
+            wp_enqueue_script("Editormd_Front", $this->front_static_url . "/assets/Editormd/editormd.min.js", array("jQuery-CDN"), WP_EDITORMD_VER, true);
+        }
 
-		//JavaScript - Config
-		wp_enqueue_script("Config_Front", $this->front_static_url . "/assets/Config/editormd.min.js", array("Editormd_Front"), $this->version, true);
+        //JavaScript - Config
+        wp_enqueue_script("Config_Front", $this->front_static_url . "/assets/Config/editormd.min.js", array("Editormd_Front"), $this->version, true);
 
-		//JavaScript - 载入国际化语言资源文件
-		$lang = get_bloginfo("language");
-		switch ($lang) {
-			case "zh-TW":
-				wp_enqueue_script("Editormd-lang-tw_Front", $this->front_static_url . "/assets/Editormd/languages/zh-tw.js", array(), WP_EDITORMD_VER, true);//载入台湾语言资源库
-				break;
-			case "zh-CN":
-				break;
-			case "en-US":
-				wp_enqueue_script("Editormd-lang-us_Front", $this->front_static_url . "/assets/Editormd/languages/en.js", array(), WP_EDITORMD_VER, true);//载入美国英语语言资源库
-				break;
-			default:
-				wp_enqueue_script("Editormd-lang-us_Front", $this->front_static_url . "/assets/Editormd/languages/en.js", array(), WP_EDITORMD_VER, true);//默认载入美国英语语言资源库
-		}
+        //JavaScript - 载入国际化语言资源文件
+        $lang = get_bloginfo("language");
+        switch ($lang) {
+            case "zh-TW":
+                wp_enqueue_script("Editormd-lang-tw_Front", $this->front_static_url . "/assets/Editormd/languages/zh-tw.js", array(), WP_EDITORMD_VER, true);
+                break;
+            case "zh-CN":
+                break;
+            case "en-US":
+                wp_enqueue_script("Editormd-lang-us_Front", $this->front_static_url . "/assets/Editormd/languages/en.js", array(), WP_EDITORMD_VER, true);
+                break;
+            default:
+                wp_enqueue_script("Editormd-lang-us_Front", $this->front_static_url . "/assets/Editormd/languages/en.js", array(), WP_EDITORMD_VER, true);
+        }
 
 
-		if ($this->get_option("highlight_library_style", "syntax_highlighting") == "customize") {
-			$prismTheme = "default";
-		} else {
-			$prismTheme = $this->get_option("highlight_library_style", "syntax_highlighting");
-		}
+        if ($this->get_option("highlight_library_style", "syntax_highlighting") == "customize") {
+            $prismTheme = "default";
+        } else {
+            $prismTheme = $this->get_option("highlight_library_style", "syntax_highlighting");
+        }
 
-		wp_localize_script("Config_Front", "Editormd", array(
-			"editormdUrl"       => $this->front_static_url, //静态资源CDN地址
-			"syncScrolling"     => $this->get_option("sync_scrolling", "editor_basics"), //编辑器同步
-			"livePreview"       => $this->get_option("live_preview", "editor_basics"), //即是否开启实时预览
-			"htmlDecode"        => $this->get_option("html_decode", "editor_basics"), //HTML标签解析
-			"toc"               => $this->get_option("support_toc", "editor_toc"), //TOC
-			"theme"             => $this->get_option("theme_style", "editor_style"), //编辑器总体主题
-			"previewTheme"      => $this->get_option("theme_style", "editor_style"), //编辑器预览主题
-			"editorTheme"       => $this->get_option("code_style", "editor_style"), //编辑器编辑主题
-			"emoji"             => $this->get_option("support_emoji", "editor_emoji"), //emoji表情
-			"tex"               => $this->get_option("support_latex", "editor_latex"), //科学公式
-			"taskList"          => $this->get_option("task_list", "editor_basics"), //task lists
-			"imagePaste"        => $this->get_option("imagepaste", "editor_basics"), //图像粘贴
-			"prismTheme"        => $prismTheme, //语法高亮风格
-			"prismLineNumbers"  => $this->get_option("line_numbers", "syntax_highlighting"), //行号显示
-			"mindMap"           => $this->get_option("support_mindmap", "editor_mindmap"), //思维导图
-			"mindMapAddres"     => $this->get_option("customize_mindmap", "editor_mindmap"), // 思维导图地址
-			"mermaid"           => $this->get_option("support_mermaid", "editor_mermaid"), // Mermaid
-			//"mermaidConfig"     => $this->get_option("mermaid_config","editor_mermaid"), // Mermaid配置
-			"placeholderEditor" => __("Enjoy Markdown! Coding now...", $this->text_domain),
-			"imgUploading"      => __("Image Uploading...", $this->text_domain),
-			"imgUploadeFailed"  => __("Failed To Upload The Image!", $this->text_domain),
-			"supportComment"    => $this->get_option("support_front", "editor_basics"), // 前端评论
-			"supportOther"      => $this->get_option("support_other_text", "editor_basics"), // 前端编辑器ID
-		));
-	}
+        wp_localize_script("Config_Front", "Editormd", array(
+            "editormdUrl"       => $this->front_static_url,                                     //静态资源CDN地址
+            "syncScrolling"     => $this->get_option("sync_scrolling", "editor_basics"),        //编辑器同步
+            "livePreview"       => $this->get_option("live_preview", "editor_basics"),          //即是否开启实时预览
+            "htmlDecode"        => $this->get_option("html_decode", "editor_basics"),           //HTML标签解析
+            "toc"               => $this->get_option("support_toc", "editor_toc"),              //TOC
+            "theme"             => $this->get_option("theme_style", "editor_style"),            //编辑器总体主题
+            "previewTheme"      => $this->get_option("theme_style", "editor_style"),            //编辑器预览主题
+            "editorTheme"       => $this->get_option("code_style", "editor_style"),             //编辑器编辑主题
+            "emoji"             => $this->get_option("support_emoji", "editor_emoji"),          //emoji表情
+            "tex"               => $this->get_option("support_latex", "editor_latex"),          //科学公式
+            "taskList"          => $this->get_option("task_list", "editor_basics"),             //task lists
+            "imagePaste"        => $this->get_option("imagepaste", "editor_basics"),            //图像粘贴
+            "prismTheme"        => $prismTheme,                                                 //语法高亮风格
+            "prismLineNumbers"  => $this->get_option("line_numbers", "syntax_highlighting"),    //行号显示
+            "mindMap"           => $this->get_option("support_mindmap", "editor_mindmap"),      //思维导图
+            "mindMapAddres"     => $this->get_option("customize_mindmap", "editor_mindmap"),    // 思维导图地址
+            "mermaid"           => $this->get_option("support_mermaid", "editor_mermaid"),      // Mermaid
+            //"mermaidConfig"     => $this->get_option("mermaid_config","editor_mermaid"),      // Mermaid配置
+            "placeholderEditor" => __("Enjoy Markdown! Coding now...", $this->text_domain),
+            "imgUploading"      => __("Image Uploading...", $this->text_domain),
+            "imgUploadeFailed"  => __("Failed To Upload The Image!", $this->text_domain),
+            "supportComment"    => $this->get_option("support_front", "editor_basics"),         // 前端评论
+            "supportOther"      => $this->get_option("support_other_text", "editor_basics"),    // 前端编辑器ID
+        ));
+    }
 
-	/**
-	 * 获取字段值
-	 *
-	 * @param string $option  字段名称
-	 * @param string $section 字段名称分组
-	 * @param string $default 没搜索到返回空
-	 *
-	 * @return mixed
-	 */
-	public function get_option($option, $section, $default = "") {
+    /**
+     * 获取字段值
+     *
+     * @param string $option  字段名称
+     * @param string $section 字段名称分组
+     * @param string $default 没搜索到返回空
+     *
+     * @return mixed
+     */
+    public function get_option($option, $section, $default = "") {
 
-		$options = get_option($section);
+        $options = get_option($section);
 
-		if (isset($options[$option])) {
-			return $options[$option];
-		}
+        if (isset($options[$option])) {
+            return $options[$option];
+        }
 
-		return $default;
-	}
+        return $default;
+    }
 
 }
