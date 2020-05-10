68b9c5fd96396471d82b97ee005813e2a99c4a04
Benjamin Chris <loli@lurenjia.in>
2020-05-10 17:34:35 +0800

    【Style】格式化assets/FrontStyle/frontstyle.js缩进为两个空格

--- assets/FrontStyle/frontstyle.js
+++ assets/FrontStyle/frontstyle.js
@@ -1,21 +1,21 @@
 (function ($, doc, win, frontStyle) {
-	$(doc).ready(function () { 
-		if (frontStyle) {
-			var conf = {
-				openLinkInNewTab: frontStyle.openLinkInNewTab === "on"
-			}
-	
-			new Handler(conf);
-		}
-	})
+  $(doc).ready(function () { 
+    if (frontStyle) {
+      var conf = {
+        openLinkInNewTab: frontStyle.openLinkInNewTab === "on"
+      }
+  
+      new Handler(conf);
+    }
+  })
 
-	function Handler(conf) {
-		// 对配置进行分别处理
-		if (conf.openLinkInNewTab) this.openLinkInNewTab();
-	}
+  function Handler(conf) {
+    // 对配置进行分别处理
+    if (conf.openLinkInNewTab) this.openLinkInNewTab();
+  }
 
-	Handler.prototype.openLinkInNewTab = function() {
-		$(".wp-editor-md-post-content-link").attr("target", "_blank");
-	}
+  Handler.prototype.openLinkInNewTab = function() {
+    $(".wp-editor-md-post-content-link").attr("target", "_blank");
+  }
 
 })(window.jQuery, document, window, window.FrontStyle);
