33c3912f7aa1f502f86e9e75982dd80790f1a0c2
Benjamin Chris <loli@lurenjia.in>
2020-05-11 20:25:38 +0800

    【Style】根据eslint提示优化assets/FrontStyle/frontstyle.js代码格式

--- assets/FrontStyle/frontstyle.js
+++ assets/FrontStyle/frontstyle.js
@@ -1,13 +1,13 @@
 (function ($, doc, win, frontStyle) {
-  $(doc).ready(function () { 
+  $(doc).ready(function() {
     if (frontStyle) {
       var conf = {
         openLinkInNewTab: frontStyle.openLinkInNewTab === "on"
-      }
-  
+      };
+
       new Handler(conf);
     }
-  })
+  });
 
   function Handler(conf) {
     // 对配置进行分别处理
@@ -16,6 +16,6 @@
 
   Handler.prototype.openLinkInNewTab = function() {
     $(".wp-editor-md-post-content-link").attr("target", "_blank");
-  }
+  };
 
 })(window.jQuery, document, window, window.FrontStyle);
