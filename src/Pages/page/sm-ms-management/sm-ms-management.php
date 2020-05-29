fb62fbe43bfb5ed0ce34133430a7a8bc99fdd151
Benjamin Chris <loli@lurenjia.in>
2020-05-29 22:37:22 +0800

    【Feat】去掉加载中的提示（没有太大意义）

--- src/Pages/page/sm-ms-management/sm-ms-management.php
+++ src/Pages/page/sm-ms-management/sm-ms-management.php
@@ -2,6 +2,5 @@
 // sm.ms图片管理页面
 
 function display_page() {
-	echo "<h1>正在加载中......</h1>";
 	wp_redirect(get_site_url() . "/wp-content/plugins/wp-editormd/src/Pages/page/sm-ms-management/html/index.html", 302);
 }
