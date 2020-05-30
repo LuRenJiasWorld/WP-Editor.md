aea28683d63327c006f9eb09b7b015fbeead7f07
Benjamin Chris <loli@lurenjia.in>
2020-05-30 17:53:47 +0800

    【Feat】避免sm-ms-management的跳转，直接获取所需HTML文件

--- src/Pages/page/sm-ms-management/sm-ms-management.php
+++ src/Pages/page/sm-ms-management/sm-ms-management.php
@@ -2,5 +2,5 @@
 // sm.ms图片管理页面
 
 function display_page() {
-	wp_redirect(get_site_url() . "/wp-content/plugins/wp-editormd/src/Pages/page/sm-ms-management/html/index.html", 302);
+	echo file_get_contents(__DIR__ . "/html/index.html");
 }
