7285dff69bc995b144a3d18ad300b6daa5a343e0
Benjamin Chris <loli@lurenjia.in>
2020-06-02 19:27:00 +0800

    【Feat】对sm-ms-management的静态资源进行分包，优化加载性能

--- src/Pages/page/sm-ms-management/vue.config.js
+++ src/Pages/page/sm-ms-management/vue.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   outputDir: "html",
   publicPath: process.env.NODE_ENV === "development" ? "/" : "../wp-content/plugins/wp-editormd/src/Pages/page/sm-ms-management/html/",
-
+  configureWebpack:{
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 200000,
+      }
+    }
+  },
   pluginOptions: {
     i18n: {
       locale: "en",
