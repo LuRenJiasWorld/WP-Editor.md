1fb44d927d966890acd0532815cdcba4f4477514
Benjamin Chris <loli@lurenjia.in>
2020-06-15 12:35:35 +0800

    【Fix】sm-ms-management.php不直接输出，返回给Caller

--- src/Pages/page/sm-ms-management/sm-ms-management.php
+++ src/Pages/page/sm-ms-management/sm-ms-management.php
@@ -2,5 +2,9 @@
 // sm.ms图片管理页面
 
 function display_page() {
-	echo file_get_contents(__DIR__ . "/html/index.html");
+	return file_get_contents(__DIR__ . "/html/index.html");
+}
+
+function wp_editormd_entry_sm_ms_proxy() {
+	return "Hello, World!";
 }
