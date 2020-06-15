fc6e48ecbb42f1df0f273f43a583643b3fbfc2e3
Benjamin Chris <loli@lurenjia.in>
2020-06-15 14:40:14 +0800

    【Feat】sm-ms-management新增代理请求功能

--- src/Pages/page/sm-ms-management/sm-ms-management.php
+++ src/Pages/page/sm-ms-management/sm-ms-management.php
@@ -5,6 +5,41 @@ function display_page() {
 	return file_get_contents(__DIR__ . "/html/index.html");
 }
 
+/**
+ * wp_editormd_entry_sm_ms_proxy()
+ * 由于sm.ms只允许后端请求，不允许前端请求，因此需要此代理
+ * 允许sm-ms-management前端在授权情况下post到此接口，并携带如下参数：
+ * - url: 代理地址
+ * - method: 代理方法(post, get)
+ * - header: 请求头(数组，不是字典)
+ * - body: 请求体(字典)
+ */
 function wp_editormd_entry_sm_ms_proxy() {
-	return "Hello, World!";
+	$postData = json_decode(file_get_contents('php://input'), true);
+
+	$url 	= $postData["url"];
+	$method = $postData["method"];
+	$header = $postData["header"];
+	$body 	= $postData["body"];
+
+	$ch     = curl_init();
+	curl_setopt($ch, CURLOPT_URL,            $url);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_POST,           $method == "post" ? 1 : 0);
+	curl_setopt($ch, CURLOPT_USERAGENT,      $user_agent);
+	curl_setopt($ch, CURLOPT_HTTPHEADER,     $header); 
+
+	if ($method == "post") {
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+	}
+
+	$result  = curl_exec($ch);
+
+	$reqCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+	header("Content-Type: application/json");
+	http_response_code($reqCode);
+	return $result;
 }
