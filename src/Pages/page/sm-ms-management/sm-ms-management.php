24ebc4200240e8c03ecb317091eb9d870b2d5298
Benjamin Chris <loli@lurenjia.in>
2020-06-15 14:40:58 +0800

    【Style】sm-ms-management.php缩进样式修正

--- src/Pages/page/sm-ms-management/sm-ms-management.php
+++ src/Pages/page/sm-ms-management/sm-ms-management.php
@@ -2,7 +2,7 @@
 // sm.ms图片管理页面
 
 function display_page() {
-	return file_get_contents(__DIR__ . "/html/index.html");
+    return file_get_contents(__DIR__ . "/html/index.html");
 }
 
 /**
@@ -15,31 +15,31 @@ function display_page() {
  * - body: 请求体(字典)
  */
 function wp_editormd_entry_sm_ms_proxy() {
-	$postData = json_decode(file_get_contents('php://input'), true);
+    $postData = json_decode(file_get_contents('php://input'), true);
 
-	$url 	= $postData["url"];
-	$method = $postData["method"];
-	$header = $postData["header"];
-	$body 	= $postData["body"];
+    $url    = $postData["url"];
+    $method = $postData["method"];
+    $header = $postData["header"];
+    $body   = $postData["body"];
 
-	$ch     = curl_init();
-	curl_setopt($ch, CURLOPT_URL,            $url);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); 
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_POST,           $method == "post" ? 1 : 0);
-	curl_setopt($ch, CURLOPT_USERAGENT,      $user_agent);
-	curl_setopt($ch, CURLOPT_HTTPHEADER,     $header); 
+    $ch     = curl_init();
+    curl_setopt($ch, CURLOPT_URL,            $url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST,           $method == "post" ? 1 : 0);
+    curl_setopt($ch, CURLOPT_USERAGENT,      $user_agent);
+    curl_setopt($ch, CURLOPT_HTTPHEADER,     $header); 
 
-	if ($method == "post") {
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
-	}
+    if ($method == "post") {
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+    }
 
-	$result  = curl_exec($ch);
+    $result  = curl_exec($ch);
 
-	$reqCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    $reqCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-	header("Content-Type: application/json");
-	http_response_code($reqCode);
-	return $result;
+    header("Content-Type: application/json");
+    http_response_code($reqCode);
+    return $result;
 }
