d9dfa42f6b14ca312c909f3a8b53d79c2e1b289d
Benjamin Chris <loli@lurenjia.in>
2020-06-16 17:10:02 +0800

    【Feat】sm-ms-management完成Token校验

--- src/Pages/page/sm-ms-management/src/App.vue
+++ src/Pages/page/sm-ms-management/src/App.vue
@@ -13,13 +13,7 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import Utils from "./utils/utils";
-
-interface Data {
-  authorize: {
-    authorize_token: String,
-    authorized: Boolean
-  }
-}
+import axios from "axios";
 
 @Component
 export default class App extends Vue {
@@ -28,11 +22,36 @@ export default class App extends Vue {
     authorized: false
   };
 
+  endpoint_url = "";
+
+  getUserInfo(): Boolean {
+    let result: Boolean = false;
+
+    axios.post(this.endpoint_url, {
+      "url": "https://sm.ms/api/v2/profile",
+      "method": "post",
+      "header": [
+        "Authorization: 8pA2q9qnaYsS9CKFifChJxQHwqRu3Y1l"
+      ],
+      "body": {}
+    })
+      .then(function(response) {
+        console.log(response.data);
+        result = true;
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+
+    return result;
+  }
+
   mounted() {
     this.$i18n.locale = Utils.getCookie("wp-editormd-lang") ? Utils.getCookie("wp-editormd-lang") : Utils.getBrowserLang();
 
     this.authorize.authorize_token = Utils.getGet("token") as string;
-    if (this.authorize.authorize_token !== "") {
+    this.endpoint_url = Utils.getGet("endpoint_url") as string;
+    if (this.authorize.authorize_token !== "" && this.endpoint_url !== "" && this.getUserInfo()) {
       this.authorize.authorized = true;
     }
 
