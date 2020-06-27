d873fbba4fc036c9c970d716bf1e238ed83a50e0
Benjamin Chris <loli@lurenjia.in>
2020-06-27 21:46:31 +0800

    【Feat】新增ImageCell组件

---
+++ src/Pages/page/sm-ms-management/src/components/ImageCell.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="image-cell">
+		<img v-bind:src="url" />
+		<p class="image-cell-file-name">{{filename}}</p>
+	</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class ImageCell extends Vue {
+	filename = "测试";
+	url = "https://i.loli.net/2020/06/26/MVS7BqwImvxLXuk.jpg";
+}
+</script>
+
+<style lang="scss">
+.image-cell {
+	width: 320px;
+	height: 240px;
+	border-radius: 12px;
+	border: 1px solid #666666;
+
+	& img {
+		width: 320px;
+		height: 180px;
+	}
+}
+</style>
