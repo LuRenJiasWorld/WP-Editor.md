efd5e23ddb4b61236654479bd442c3de4066eadf
Benjamin Chris <loli@lurenjia.in>
2020-06-29 22:58:30 +0800

    【Chore】sm-ms-management更新依赖

--- src/Pages/page/sm-ms-management/src/main.ts
+++ src/Pages/page/sm-ms-management/src/main.ts
@@ -1,9 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
-import { Button } from "view-design";
+import { Button, ButtonGroup, Divider, Input, Card, Tooltip } from "view-design";
 import "view-design/dist/styles/iview.css";
 import i18n from "./i18n";
+import "./theme/view-ui-theme.less";
 
 console.log(process.env);
 
@@ -13,6 +14,11 @@ if (process.env.VUE_APP_MOCK === "mock") {
 }
 
 Vue.component("Button", Button);
+Vue.component("ButtonGroup", ButtonGroup);
+Vue.component("Divider", Divider);
+Vue.component("Input", Input);
+Vue.component("Card", Card);
+Vue.component("Tooltip", Tooltip);
 
 Vue.config.productionTip = false;
 
