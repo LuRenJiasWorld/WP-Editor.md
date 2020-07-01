c5d9345d962c0bbc408d13b5579a2e6288929665
Benjamin Chris <loli@lurenjia.in>
2020-07-01 23:43:31 +0800

    【Feat】sm-ms-management完善image_list数据及其mutation

--- src/Pages/page/sm-ms-management/src/store/index.ts
+++ src/Pages/page/sm-ms-management/src/store/index.ts
@@ -1,12 +1,50 @@
 import Vue from "vue";
 import Vuex from "vuex";
+// 此处存在误判，实际变量被使用，但提示未使用
+// eslint-disable-next-line
+import { ImageInfoType, StateType } from "./index.type";
 
 Vue.use(Vuex);
 
+let state: StateType = {
+  image_list: []
+};
+
 export default new Vuex.Store({
-  state: {
-  },
+  state: state,
   mutations: {
+    getImage(state, imageHash: string): ImageInfoType | null {
+      let imageInfo: ImageInfoType | null = null;
+
+      state.image_list.forEach((element: ImageInfoType) => {
+        if (element.hash === imageHash) {
+          imageInfo = element;
+        }
+      });
+
+      return imageInfo;
+    },
+    getImages(state): ImageInfoType[] {
+      return state.image_list;
+    },
+    addImage(state, imageInfo: ImageInfoType): boolean {
+      state.image_list = [...state.image_list, imageInfo];
+      return true;
+    },
+    addImages(state, imageInfoBatch: ImageInfoType[]): boolean {
+      state.image_list = [...state.image_list, ...imageInfoBatch];
+      return true;
+    },
+    deleteImage(state, imageHash: string): boolean {
+      let success = false;
+      state.image_list.forEach((element, index, object) => {
+        if (element.hash === imageHash) {
+          object.splice(index, 1);
+          success = true;
+        }
+      });
+      return success;
+    }
   },
   actions: {
   },
