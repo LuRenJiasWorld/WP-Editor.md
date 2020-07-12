9af6243e52b418332ea9f5361d520aa28741e599
Benjamin Chris <loli@lurenjia.in>
2020-07-12 23:29:08 +0800

    【Perf】优化单行LaTeX解析流程，避免各种可能会存在的BUG

--- src/App/KaTeX.php
+++ src/App/KaTeX.php
@@ -48,54 +48,56 @@ class KaTeX {
 
         // 初始化参数
         $count = 0;
-        $preg  = true;
+        // 是否需要跳过LaTeX解析
+        $pass  = false;
 
         foreach ($textarr as &$element) {
-
-            //判断是否在code里面
+            // 判断已经跳过的行数
             if ($count > 0) {
                 ++ $count;
             }
 
+            /**
+             * 1. 判断是否满足如下规则，如果是则不进行LaTeX解析
+             * <pre>
+             * </pre>
+             */
             // 判断是否是<pre>然后开始计数，此时为第一行
             if (htmlspecialchars_decode($element) == "<pre>") {
                 $count = 1;
-            }
-
-            // 当读到第三行时，判断是code标签嘛，如果是，说明是代码，则后续不进行处理
-            if ($count == 3 && strpos(htmlspecialchars_decode($element), "<code class=") === 0) {
-                $preg = false;
+                $pass = true;
             }
 
             // 如果发现是</pre>标签，则表示代码部分结束，继续处理
             if (htmlspecialchars_decode($element) == "</pre>") {
-                $preg = true;
+                $pass = false;
             }
 
+            /**
+             * 2. 对于使用```katex的多行LaTeX，不在里面进行LaTeX的重复解析
+             */
             if (strpos(htmlspecialchars_decode($element), '<div class="katex math') === 0) {
                 $count = 1;
-                $preg = false;
+                $pass = true;
             }
             
             if ($count == 3 && htmlspecialchars_decode($element) == "</div>") {
-                $preg = true;
+                $pass = false;
             }
 
-            // 如果在代码中，则跳出本次循环
-            if (! $preg) {
+            /**
+             * 3. 对于其他空行或可能为HTML单行标签的行，直接跳过
+             */
+            if ($element == "" || $element[0] == "<" || !stripos($element, "$")) {
+                $pass = true;
+            }
+
+            // 如果存在需要跳过LaTeX解析的情况，在这里跳过
+            if ($pass) {
                 continue;
+            } else {
+                $element = preg_replace_callback($regexTeXInline, array($this, "katex_src_inline"), $element);
             }
-
-            // 跳出循环
-            if ("" === $element || "<" === $element[0]) {
-                continue;
-            }
-
-            if (false === stripos($element, "$")) {
-                continue;
-            }
-
-            $element = preg_replace_callback($regexTeXInline, array($this, "katex_src_inline"), $element);
         }
 
         return implode("", $textarr);
