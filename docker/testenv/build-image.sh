d43983a82f60582ab6976522e95656ef4be12d3a
Benjamin Chris <loli@lurenjia.in>
2021-11-30 23:03:38 +0800

    feat(docker): 测试环境构建脚本支持代理，加快构建速度

--- docker/testenv/build-image.sh
+++ docker/testenv/build-image.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-
+begin_time=$(date +%s)
 version=$(date +%Y-%m-%d-%H-%M-%S)
 
-sudo docker build . -t="wp-editormd/testenv:$version"
-sudo docker tag "wp-editormd/testenv:$version" "wp-editormd/testenv:latest"
+sudo docker build \
+    --build-arg HTTP_PROXY=$HTTP_PROXY   --build-arg http_proxy=$http_proxy   \
+    --build-arg HTTPS_PROXY=$HTTPS_PROXY --build-arg https_proxy=$https_proxy \
+    --build-arg NO_PROXY=$NO_PROXY       --build-arg no_proxy=$no_proxy       \
+    . -t="wp-editormd/testenv:$version" \
+&& sudo docker tag "wp-editormd/testenv:$version" "wp-editormd/testenv:latest"
+
+end_time=$(date +%s)
+
+echo "任务完成，总耗时$(($end_time-$begin_time))秒！"
